477acf42ffaf5dac76a6bceca3ddb13af25d5481
Anastasiia Semeniuk <145162711+AnastasiiaSemeniukk@users.noreply.github.com>
2023-10-18 18:51:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
- document.addEventListener('DOMContentLoaded', function () {
-            const header = document.querySelector('.header');
-            const logo = document.querySelector('.logo');
-            const contactbtn = document.getElementById('contactbtn');
-            const navLinks = document.querySelectorAll('.nav-links a');
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
 
-            const headerHeight = header.offsetHeight; 
-            const firstPageHeight = document.getElementById('page0').offsetHeight; 
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
 
-            window.addEventListener('scroll', function () {
-                if (window.scrollY > firstPageHeight - headerHeight) {
-                    header.style.backgroundColor = '#736f73';
-                    logo.src = 'img/logo2.png';
-                    contactbtn.style.backgroundColor = '#272021';
-                    navLinks.forEach(link => {
-                        link.style.color = '#272021';
-                    });
-                } else if (window.scrollY > 50) {
-                    header.style.backgroundColor = '#272021';
-                    logo.src = 'img/logo.png';
-                    contactbtn.style.backgroundColor = '#abb1b9';
-                    navLinks.forEach(link => {
-                        link.style.color = '#e5f1f6';
-                    });
-                    
-                }
-            });
-        });
+window.addEventListener("scroll", function() {
+    var header = document.querySelector('header');
+    var scrolled = window.scrollY;
+    var firstPageHeight = document.getElementById('mainpage').offsetHeight;
+    var headerHeight = header.offsetHeight;
+
+    if (scrolled > firstPageHeight - headerHeight) {
+        header.style.backgroundColor = '#272021';
+    } else {
+        header.style.backgroundColor = '#736f73';
+    }
+});
+
 
 
 document.getElementById('contactbtn').addEventListener('click', function() {
-    // Обробка подій при натисканні кнопки, наприклад, прокрутка до секції контактів
     document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
 });
 
@@ -74,47 +66,21 @@ document.getElementById('contactbtn').addEventListener('click', function() {
         const toTop = document.querySelector(".to-top");
 
         window.addEventListener("scroll", () => {
-          if (window.pageYOffset > 100) {
-            toTop.classList.add("active");
-          } else {
-            toTop.classList.remove("active");
-          }
+            if (window.pageYOffset > 100) {
+                toTop.classList.add("active");
+            } else {
+                toTop.classList.remove("active");
+            }
         })
 
 
         function callNumber(phoneNumber) {
             window.location.href = 'tel:' + phoneNumber;
         }
-        
-
-      
 
 
-        
-
-        
-        
-        
-       
-        
-        
-        
-        
 
 
-        
 
-
-        
             
-
-      
-     
-      
-      
-      
-      
-      
-      
-
-
+  
