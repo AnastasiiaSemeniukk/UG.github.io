f299372fdfc900b9674d9ccf6c98c22314f8dd54
AnastasiiaSemeniukk <sandersstacy02@gmail.com>
2024-05-21 23:09:47 +0300

    Adding new slider and works whoto

--- script.js
+++ script.js
@@ -1,59 +1,38 @@
-const gallery = document.querySelector('.gallery')
-const slides = document.querySelectorAll('.slide')
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
 
-let activeSlide = 0
-
-rightBtn.addEventListener('click', () => {
-  activeSlide++
-
-  if (activeSlide > slides.length - 1) {
-    activeSlide = 0
-  }
-
-  setBgToGallery()
-  setActiveSlide()
-})
-
-leftBtn.addEventListener('click', () => {
-  activeSlide--
-
-  if (activeSlide < 0) {
-    activeSlide = slides.length - 1
-  }
-
-  setBgToGallery()
-  setActiveSlide()
-})
-
-setBgToGallery()
-
-function setBgToGallery() {
-  gallery.style.backgroundImage = slides[activeSlide].style.backgroundImage
-}
-
-function setActiveSlide() {
-  slides.forEach((slide) => slide.classList.remove('active'))
-
-  slides[activeSlide].classList.add('active')
-}
-
-
+// Menu link functionality
 const menuLinks = document.querySelectorAll('.navbar a');
-
 
 menuLinks.forEach(link => {
   link.addEventListener('click', () => {
     menuLinks.forEach(item => {
       item.classList.remove('active');
     });
-
     link.classList.add('active');
   });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    
+  });
+});
 
+
+
+
+
+// Function to call a phone number
 function callNumber(phoneNumber) {
     window.location.href = 'tel:' + phoneNumber;
 }
+
+
